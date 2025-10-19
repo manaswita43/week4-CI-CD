8794778febb4c0fbe9b50da16bd8c0677e47695c
manaswita43 <manaswita43@gmail.com>
2025-10-19 16:09:57 +0530

    Update test_data_validation.py

--- tests/test_data_validation.py
+++ tests/test_data_validation.py
@@ -6,4 +6,4 @@ def test_data_shape():
 
 def test_no_missing_values():
     df = pd.read_csv("data/iris.csv")
-    assert df.isnull().sum().sum() == 0, "Dataset contains missing values"
+    assert df.isnull().sum().sum() == 0, "Dataset contains missing values."
